e63f61f7122d54709003ce715b2eee9218d68eb5
Jon Nguy <jpn.guyy@gmail.com>
2013-06-02 23:02:43 -0700

    main.cpp updated with Music stuff

--- main.cpp
+++ main.cpp
@@ -2,9 +2,47 @@
 // GLUT harness v. 1.0
 // ------------------------
 
+
+#ifdef __APPLE__
+#include "fmod.hpp" //fmod c++ header
+#include "fmod_errors.h"
+#include <sys/time.h>
+
+// Music stuff
+FMOD::System            *fmodSystem;
+FMOD::Sound             *song;
+FMOD::Channel           *channel = 0;
+FMOD::ChannelGroup      *channelgroup;
+FMOD::DSP               *dsp = 0, *dsphead;
+FMOD::DSPConnection     *dspconnection = 0;
+FMOD_RESULT             result;
+unsigned int            version;
+void                    *extradriverdata = 0;
+bool musicPaused = true;
+
+// getSpectrum() performs the frequency analysis, see explanation below
+int sampleSize = 256;
+
+float *specLeft, *specRight, *spec;
+char *valueString;
+
+// Beat threasholds
+float beatThresholdVolumeSmall = 0.3f;
+float beatThresholdVolumeMedium = 0.6f;
+float beatThresholdVolumeLarge = 0.9f;
+int beatThresholdBar = 1;            // The bar in the volume distribution to examine
+unsigned int beatPostIgnore = 250;   // Number of ms to ignore track for after a beat is recognized
+int beatLastTick = 0;                // Time when last beat occurred
+#endif
+
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#ifdef __APPLE__ 
+#include <GL/glew.h>
+#endif
 #include "Angel.h"
 #include "MyFunctions.h"
 #include "sphere.h"
@@ -120,6 +158,32 @@ void myInit( void )
 	//* BACKGROUND INITIALIZE ********
 	//********************************
 	glClearColor( 0.0, 0.0, 0.0, 1.0 );		// Black Background
+    
+    
+#ifdef __APPLE__
+    // MUSIC
+    result = FMOD::System_Create(&fmodSystem);
+    
+    result = fmodSystem->getVersion(&version);
+    result = fmodSystem->init(32, FMOD_INIT_NORMAL, extradriverdata);
+    result = fmodSystem->createSound("brave.mp3", FMOD_SOFTWARE | FMOD_LOOP_NORMAL , 0, &song);
+    
+    result = fmodSystem->playSound(FMOD_CHANNEL_FREE, song, true, &channel);
+    //    result = fmodSystem->playSound(song, 0, true, &channel);
+    fmodSystem->update(); // Per-frame FMOD update ('system' is a pointer to FMOD::System)
+    
+    
+    // For spectrum analyzer
+    specLeft = new float[sampleSize];
+    specRight = new float[sampleSize];
+    spec = new float[sampleSize];
+    
+    for (int i = 0; i<sampleSize; i++){
+        specLeft[i] = 0.0;
+        specRight[i] = 0.0;
+        spec[i] = 0.0;
+    }
+#endif
 }
 
 void spawn_sphere()
@@ -135,6 +199,7 @@ void draw_sphere()
 	// Background
 	glUniform1i( sphereID, 0 );
 	setScale( 1000 );
+    setColor( 0, 0, 0, 1, 1 );
 	setTranslation( 0, 0, 0 );
 	spawn_sphere();
 
@@ -151,9 +216,31 @@ void draw_sphere()
 
 	// Right Sphere
 	glUniform1i( sphereID, 1 );
+	setColor( 0.5, 0.87, 0.13, 1.0, 1.0 );
+	setScale( 10 );
+	setTranslation( 30, 0, 0 );
+	// Lighting
+	glUniform4fv( fMaterialAmbient, 1, ambient_product );
+    glUniform4fv( fMaterialDiffuse, 1, diffuse_product );
+    glUniform4fv( fMaterialSpecular, 1, specular_product );
+	spawn_sphere();
+    
+    // Far Sphere
+	glUniform1i( sphereID, 1 );
 	setColor( 0.6, 0.27, 0.83, 1.0, 1.0 );
 	setScale( 14 );
-	setTranslation( 30, 0, 0 );
+	setTranslation( 0, 0, 30 );
+	// Lighting
+	glUniform4fv( fMaterialAmbient, 1, ambient_product );
+    glUniform4fv( fMaterialDiffuse, 1, diffuse_product );
+    glUniform4fv( fMaterialSpecular, 1, specular_product );
+	spawn_sphere();
+    
+    // Front Sphere
+	glUniform1i( sphereID, 1 );
+	setColor( 0.6, 0.27, 0.83, 1.0, 1.0 );
+	setScale( 14 );
+	setTranslation( 0, 0, -30 );
 	// Lighting
 	glUniform4fv( fMaterialAmbient, 1, ambient_product );
     glUniform4fv( fMaterialDiffuse, 1, diffuse_product );
@@ -168,9 +255,14 @@ void draw_sphere()
 void initGlut(int& argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitContextVersion( 3, 2 );
-    glutInitContextProfile( GLUT_CORE_PROFILE );
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    #ifdef __APPLE__  // include Mac OS X verions of headers
+        glutInitDisplayMode(GLUT_3_2_CORE_PROFILE | GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH );
+    #else // non-Mac OS X operating systems
+        glutInitContextVersion( 3, 2 );
+        glutInitContextProfile( GLUT_CORE_PROFILE );
+        glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    #endif  // __APPLE__
+    
 	glutInitWindowPosition(400, 0);
 	glutInitWindowSize(winWidth, winHeight);
 	glutCreateWindow("Music Visualizer - Hao, Nguy, Sabatine");
@@ -189,6 +281,78 @@ void initGlut(int& argc, char** argv)
 // TODO: Don't forget to add glutPostRedisplay().
 void callbackDisplay()
 {
+    
+#ifdef __APPLE__
+    // Per-frame FMOD update ('system' is a pointer to FMOD::System)
+    fmodSystem->update();
+    //        std::transform(&spec[0], &spec[sampleSize], &spec[0], normalize);
+    //[maxVol] (float dB) -> float { return dB / maxVol; });
+    //    if (!musicPaused)
+    //        printf("%f \n", maxVol);
+    
+    // Get spectrum for left and right stereo channels
+    channel->getSpectrum(specLeft, sampleSize, 0, FMOD_DSP_FFT_WINDOW_RECT);
+    channel->getSpectrum(specRight, sampleSize, 1, FMOD_DSP_FFT_WINDOW_RECT);
+    
+    // Find max volume
+    auto maxIterator = std::max_element(&spec[0], &spec[sampleSize]);
+    float maxVol = *maxIterator;
+    
+    // Normalize
+    if (maxVol != 0)
+        for (int i = 0; i < sampleSize; i++)
+            spec[i] = spec[i]/maxVol;
+    //
+    //    if (!musicPaused)
+    //        printf("Max vol: %f \n", maxVol);
+    
+    if (!musicPaused) {
+        for (int i = 0; i < sampleSize; i++){
+            spec[i] = (specLeft[i] + specRight[i]) / 2;
+            
+            if (spec[i] > .3){
+            printf("Left %i: %f \n", i, specLeft[i]);
+            printf("Right %i: %f \n", i, specRight[i]);
+            //            if (spec[i] > .15)
+            //                printf("%i: %f \n", i, spec[i]);
+            }
+        }
+        
+        bool beatDetectedSmall = false;
+        bool beatDetectedMedium = false;
+        bool beatDetectedLarge = false;
+        
+        timeval t;
+        
+        
+        // Test for threshold volume being exceeded (if not currently ignoring track)
+        if ((spec[beatThresholdBar] >= beatThresholdVolumeLarge ||
+             spec[beatThresholdBar+1] >= beatThresholdVolumeLarge) && beatLastTick == 0)
+        {
+            beatLastTick = gettimeofday(&t,NULL);
+            beatDetectedLarge = true;
+        } else if ((spec[beatThresholdBar] >= beatThresholdVolumeMedium ||
+                    spec[beatThresholdBar+1] >= beatThresholdVolumeMedium) && beatLastTick == 0) {
+            beatLastTick = gettimeofday(&t,NULL);
+            beatDetectedMedium = true;
+        } else if ((spec[beatThresholdBar] >= beatThresholdVolumeSmall ||
+                    spec[beatThresholdBar+1] >= beatThresholdVolumeSmall) && beatLastTick == 0) {
+            beatLastTick = gettimeofday(&t,NULL);
+            beatDetectedSmall = true;
+        }
+        
+        if (beatDetectedLarge) {
+        } else if (beatDetectedMedium) {
+        } else if (beatDetectedSmall) {
+            
+        }
+        
+        // If the ignore time has expired, allow testing for a beat again
+        if (gettimeofday(&t,NULL) - beatLastTick >= beatPostIgnore)
+            beatLastTick = 0;
+    }
+#endif
+    
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// Update Camera
@@ -197,6 +361,7 @@ void callbackDisplay()
 	// Draw the main sphere
 	setColor( 1, 1, 1, 1.0 , 1 );
 	draw_sphere();
+    
 
 	glutSwapBuffers();
 }
@@ -239,7 +404,17 @@ void callbackKeyboard(unsigned char key, int x, int y)
 				camera.angleTheta -= 2*M_PI;
 			camera.updateCameraPos();
 			break;
-		
+            
+#ifdef __APPLE__
+        case 'p':
+            // Play or pause the sound
+            if (!musicPaused){
+                result = channel->setPaused(musicPaused = true);
+            } else {
+                result = channel->setPaused(musicPaused = false);
+            }
+            break;
+#endif
 		case ' ':
 			camera.resetCamera();
 			camera.updateCameraPos();
